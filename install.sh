ff1caa6246f0362b1f5289c9480bb8da20c0438d
D'Tor Aelek <107276233+dtoraelek@users.noreply.github.com>
2026-08-09 05:41:13 +0000

    Update install.sh
    Added Qt6 flag because of Qt5 no longer being maintained

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir build
    cd build
-   cmake ../
+   cmake -DWANT_QT6=ON ../ || Please ensure you have met all dependencies before compiling and try again.
    make
    sudo make install || doas make install || echo "Please install either sudo or doas to install. Without propper configuration, this message may also appear."
